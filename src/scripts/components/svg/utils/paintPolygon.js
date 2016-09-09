89a5f6933be2cc36ce1d242365fe1aa03fcb0b9f
andrey <casumarzu@yandex.ru>
2016-09-09 17:58:11 +0300

    Change paintPolygon code

--- src/scripts/components/svg/utils/paintPolygon.js
+++ src/scripts/components/svg/utils/paintPolygon.js
@@ -1,18 +1,17 @@
 export default function paintPolygon(...args) {
   let result = ''
   if(args.length % 2 === 0) {
-    const middle = [...args]
-    middle.splice(0, 1)
-    middle.splice(-1, 1)
-    const middleStr = middle.reduce( (result, item, i) => {
-      if(i === 1 || i % 2) {
-        result += ' ' + item
-      }else {
-        result += ',' + item
-      }
-      return result
+    const middleArr = [...args]
+    middleArr.splice(0, 1)
+    middleArr.splice(-1, 1)
+    const middle = middleArr.reduce( (result, item, i) => {
+      let seporator = ','
+      if(i === 1 || i % 2) seporator = ' '
+      return [result, seporator, item].join('')
     } )
-    result = args[0] + ',' + middleStr + ',' + args[args.length - 1]
+    const first = args[0]
+    const last = args[args.length - 1]
+    result = `${first},${middle},${last}`
   }else {
     throw new Error('the number of arguments must be even')
   }
